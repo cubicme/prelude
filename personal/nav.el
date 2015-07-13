f3a1bb236e0fc920323b8054b359b666e4e12a18
cubicme <sa.monemian@gmail.com>
2015-07-13 08:21:56 +0430

    use arrow buttons in nav window

--- personal/nav.el
+++ personal/nav.el
@@ -1,2 +1,8 @@
 (require 'nav)
 (global-set-key (kbd "C-\\") 'nav-toggle)
+
+(defun nav-mode-hl-hook ()
+  (local-set-key (kbd "<right>") 'nav-open-file-under-cursor)
+  (local-set-key (kbd "<left>")  'nav-go-up-one-dir))
+
+(add-hook 'nav-mode-hook 'nav-mode-hl-hook)
